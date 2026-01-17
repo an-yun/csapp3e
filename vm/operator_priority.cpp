6455165a76bc42c48c666a8b1a5163e59f25e35d
an-yun <zsy296@gmail.com>
2026-01-17 10:52:08 +0800

    增加运算符优先级验证代码

---
+++ vm/operator_priority.cpp
@@ -0,0 +1,28 @@
+//
+// Created by zuo on 2026/1/17.
+//
+
+#include <iostream>
+
+void print(const int* arr, size_t size) {
+    using std::cout;
+    cout << "the address of arr is" << arr << std::endl;
+    cout << "the data is" << std::endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << std::endl;
+}
+
+int main() {
+    size_t size = 4;
+    int* original_arr = new int[size]{};
+    int* arr = original_arr;
+    print(arr, size);
+    *arr++;
+    print(arr, size);
+    (*arr)++;
+    print(arr, size);
+    delete[] original_arr;
+    return 0;
+}
